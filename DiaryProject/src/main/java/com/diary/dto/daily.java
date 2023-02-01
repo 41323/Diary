eb70e4ce98d1806b31122fe48c31baa5253efd71
SY <pj7234@naver.com>
2023-02-01 15:30:13 +0900

    dto daily 추가

---
+++ DiaryProject/src/main/java/com/diary/dto/daily.java
@@ -0,0 +1,13 @@
+package com.diary.dto;
+
+import java.sql.Date;
+
+public class daily {
+	int seq_d; //고유번호
+	String map_d;//지도 데이터
+	String content_d;//내용
+	String image_d;//이미지
+	String memo_d;//메모
+	Date regdate_d;//작성일
+
+}
